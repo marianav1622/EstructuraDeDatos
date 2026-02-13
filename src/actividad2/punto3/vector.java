441398c6ff95aa47d50df58328ed5284c268ac04
Mariana <mvreyesp@libertadores.edu.co>
2026-02-13 02:40:36 +0000

    cambios banco

--- src/actividad2/punto3/vector.java
+++ src/actividad2/punto3/vector.java
@@ -1,1 +1,16 @@
-import java.util.vector
+import java.util.Vector;
+
+public class vector{
+   public static void main(String[]args){
+
+        Vector<String> logTransacciones = new Vector<>();
+        logTransacciones.add("pago");
+        logTransacciones.add("Deposito");
+        logTransacciones.add("Retiro");
+        logTransacciones.add("trasaccion");
+        logTransacciones.add("abono");
+        //**los métodos de Vector son synchronized, lo que bloquea el objeto para otros hilos mientras uno lo usa esto garantiza que solo un hilo pueda acceder al Vector a la vez */
+   
+    } 
+}
+
