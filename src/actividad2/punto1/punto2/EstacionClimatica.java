c9446b1870895f32cff3ba1eb3b1b082c6d03aab
Mariana <mvreyesp@libertadores.edu.co>
2026-02-13 00:27:41 +0000

    nuevo clima

---
+++ src/actividad2/punto1/punto2/EstacionClimatica.java
@@ -0,0 +1,17 @@
+public class EstacionClimatica{
+    public static void main(String []args){
+        double[] temperaturas =
+new double[24];
+
+for (int i=0 i<= temperatura.length; i++){
+    System.out.println(temperaturas[i] = Math.random() * 40;)
+}
+System.out.println("temperatura al medio dia (12 horas: )" + temperatura[12];
+tyr{
+    temperatura [24](hora 25)
+}
+catch( ArrayIndexOutOfBoundsException){
+     System.out.println("No se puede acceder a la posición 24 porque está fuera de los límites.");
+}
+    }
+}
