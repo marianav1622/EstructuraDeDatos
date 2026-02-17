9a186e2cc401b8ce9f25b68caafeb1671d9f7762
Mariana <mvreyesp@libertadores.edu.co>
2026-02-17 02:34:08 +0000

    cambiosacti5

---
+++ src/actividad2/punto5/cine.java
@@ -0,0 +1,13 @@
+public class cine{
+    public static void main(String[]args){
+        private int[][] sala = new int[8][10];
+        public void inicializar(){ 
+         for(i=0; i<7; i++){
+            for(j=0; j<1; j++){
+                sala[i][j]=0;
+            }
+         }
+
+       
+    }
+}
